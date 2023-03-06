4f028a4acf3d0636be2a68afe55cd2775fa78cb7
Naithar01 <eeung025@gmail.com>
2023-03-06 23:22:10 +0900

    docker-compose 서버와 DB가 연결되지 않는 버그 수정

--- db/migration.sql
+++ db/migration.sql
@@ -1,9 +1,19 @@
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'snmsung1';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'snmsung1';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+
+FLUSH PRIVILEGES;
+
+USE writedream;
+
 CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title_UNIQUE` (`title`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `issues` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -13,7 +23,7 @@ CREATE TABLE `issues` (
   `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `memos` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -23,7 +33,7 @@ CREATE TABLE `memos` (
   PRIMARY KEY (`id`),
   KEY `issue_id` (`issue_id`),
   CONSTRAINT `memos_ibfk_1` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`id`) ON DELETE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `issue_category` (
   `issue_id` int NOT NULL,
@@ -33,6 +43,6 @@ CREATE TABLE `issue_category` (
   KEY `idx_category` (`category_id`),
   CONSTRAINT `iss_cate_ibfk_1` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`id`) ON DELETE CASCADE,
   CONSTRAINT `iss_cate_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO writedream.categories (`title`) VALUES ('Default');
